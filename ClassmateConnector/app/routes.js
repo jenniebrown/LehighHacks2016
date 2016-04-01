1f80ffe51604d3e2d91e3fdd0f7ada68a93b8009
alw218 <amberw95@gmail.com>
2016-04-01 10:21:22 -0400

    changes for chat

--- ClassmateConnector/app/routes.js
+++ ClassmateConnector/app/routes.js
@@ -4,9 +4,23 @@ module.exports = function(app, passport) {
 	// =====================================
 	// HOME PAGE (with login links) ========
 	// =====================================
-	app.get('/', function(req, res) {
+	var app = require('express')();
+	var http = require('http').Server(app);
+	var io = require('socket.io')(http);
+
+	app.get('/', function(req, res){
 		res.render('landingpage.ejs', {"classes":["CSE241","CSE271","CSE261"]}); //index.ejs'); // load the index.ejs file
 	});
+
+	io.on('connection', function(socket){
+	  socket.on('chat message', function(msg){
+	    io.emit('chat message', msg);
+	  });
+	});
+	
+	/*app.get('/', function(req, res) {
+		res.render('landingpage.ejs', {"classes":["CSE241","CSE271","CSE261"]}); //index.ejs'); // load the index.ejs file
+	});*/
 
 	// =====================================
 	// LOGIN ===============================
