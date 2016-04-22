145b9d0d8b1c5121b20f09d78fce3960dc6def17
jlb315 <jlb315@lehigh.edu>
2016-04-22 02:16:24 -0400

    safe config

--- ClassmateConnector/config/auth.js
+++ ClassmateConnector/config/auth.js
@@ -3,9 +3,9 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     'googleAuth' : {
-        'clientID'      : '770645782638-1mkb0tittri4mu72q5plqu5umhqu90p8.apps.googleusercontent.com',
-        'clientSecret'  : 'b3VrtMqoj-Y5j8jGMhcQfjW_',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'clientID'      : '<clientID>.apps.googleusercontent.com',
+        'clientSecret'  : '<clientSecret>',
+        'callbackURL'   : '<callbackURL>'
     }
 
 };
--- ClassmateConnector/config/database.js
+++ ClassmateConnector/config/database.js
@@ -1,8 +1,9 @@
 module.exports = {
 
-    host : 'us-cdbr-iron-east-03.cleardb.net', 
-    user : 'b71d1117f536dd',
-    password : '8a587629'
+    host : '<hostname>', 
+    user : '<username>',
+    password : '<password>',
+    database : '<database>'
 
 
 };
